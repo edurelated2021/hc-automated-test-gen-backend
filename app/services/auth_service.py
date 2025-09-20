efa67000d3f0f79749158ed51f83e1b51e0bb762
Bahniman <edurelated2021@gmail.com>
2025-09-20 20:29:24 +0530

    Delete app/services directory

--- app/services/auth_service.py
+++
@@ -1,5 +0,0 @@
-
-# Very basic auth service for demo purposes only.
-class AuthService:
-    def authenticate(self, username: str, password: str) -> bool:
-        return username == 'admin' and password == 'default'
